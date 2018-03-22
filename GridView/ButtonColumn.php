80299bce7bb3dba9c81f0ab10564dc839041dbd9
Sergei Pavlov <spavlov@li.ru>
2018-03-22 11:42:55 +0700

    Improve docs

--- GridView/ButtonColumn.php
+++ GridView/ButtonColumn.php
@@ -25,6 +25,10 @@ use yii\web\ServerErrorHttpException;
  *          'urlCreator' => function ($action, $model, $key, $index) {
  *              return Url::to(['/controller/action','id'=>$key]);
  *          }
+ *          #You can make the button disabled using callback function. Is optional.
+ *          'disabled' => function ($model) {
+ *              return false;
+ *          }
  *     ],
  * ]
  * ```
@@ -32,107 +36,107 @@ use yii\web\ServerErrorHttpException;
  * @author SPavlov
  */
 class ButtonColumn extends ActionColumn {
-	/**
-	 * @var boolean Disabled if true
-	 */
-	public $disabled;
+    /**
+     * @var boolean Disabled if true
+     */
+    public $disabled;
 
-	public $iconCssClass;
-	public $btnCssClass;
-	public $action;
-	public $title;
-	public $data = [];
+    public $iconCssClass;
+    public $btnCssClass;
+    public $action;
+    public $title;
+    public $data = [];
 
     /**
      * @var string type of button
      */
-	public $type = 'link';
+    public $type = 'link';
 
     /**
      * @var array Style definition
      */
-	public $contentOptions = [ 'style' => 'width: 80px;text-align:center;' ];
+    public $contentOptions = ['style' => 'width: 80px;text-align:center;'];
 
 
-	protected function renderHeaderCellContent() {
-		return '<div class="text-center"><i class="' . $this->iconCssClass . '"></i></div>';
-	}
+    protected function renderHeaderCellContent() {
+        return '<div class="text-center"><i class="' . $this->iconCssClass . '"></i></div>';
+    }
 
-	public function init() {
+    public function init() {
 
-	    parent::init();
+        parent::init();
 
-		if ( ! $this->action && ! $this->urlCreator ) {
-			throw new ServerErrorHttpException( 'Either @action attribute or @urlCreator should be set.' );
-		}
+        if (!$this->action && !$this->urlCreator) {
+            throw new ServerErrorHttpException('Either @action attribute or @urlCreator should be set.');
+        }
 
-		if ( ! $this->action ) {
-			$this->action = uniqid();
-		} elseif ( $this->action == 'delete' ) {
-			$this->data['confirm'] = Yii::t( 'yii', 'Are you sure you want to delete this item?' );
-			$this->data['method']  = 'POST';
-			$this->btnCssClass     = ! $this->btnCssClass ? 'btn btn-sm btn-small btn-danger' : $this->btnCssClass;
-			$this->iconCssClass    = ! $this->iconCssClass ? 'far fa-trash-alt' : $this->iconCssClass;
-		} elseif ( $this->action == 'view' ) {
-			$this->btnCssClass  = ! $this->btnCssClass ? 'btn  btn-sm btn-small btn-primary' : $this->btnCssClass;
-			$this->iconCssClass = ! $this->iconCssClass ? 'fas fa-eye' : $this->iconCssClass;
-		} elseif ( $this->action == 'update' ) {
-			$this->btnCssClass  = ! $this->btnCssClass ? 'btn  btn-sm btn-small btn-success' : $this->btnCssClass;
-			$this->iconCssClass = ! $this->iconCssClass ? 'far fa-edit' : $this->iconCssClass;
-		} elseif ( $this->action == 'create' ) {
-			$this->btnCssClass  = ! $this->btnCssClass ? 'btn  btn-sm btn-small btn-primary' : $this->btnCssClass;
-			$this->iconCssClass = ! $this->iconCssClass ? 'fas fa-plus' : $this->iconCssClass;
-		}
-		$this->initMyCustomButton();
-	}
+        if (!$this->action) {
+            $this->action = uniqid();
+        } elseif ($this->action == 'delete') {
+            $this->data['confirm'] = Yii::t('yii', 'Are you sure you want to delete this item?');
+            $this->data['method'] = 'POST';
+            $this->btnCssClass = !$this->btnCssClass ? 'btn btn-sm btn-small btn-danger' : $this->btnCssClass;
+            $this->iconCssClass = !$this->iconCssClass ? 'far fa-trash-alt' : $this->iconCssClass;
+        } elseif ($this->action == 'view') {
+            $this->btnCssClass = !$this->btnCssClass ? 'btn  btn-sm btn-small btn-primary' : $this->btnCssClass;
+            $this->iconCssClass = !$this->iconCssClass ? 'fas fa-eye' : $this->iconCssClass;
+        } elseif ($this->action == 'update') {
+            $this->btnCssClass = !$this->btnCssClass ? 'btn  btn-sm btn-small btn-success' : $this->btnCssClass;
+            $this->iconCssClass = !$this->iconCssClass ? 'far fa-edit' : $this->iconCssClass;
+        } elseif ($this->action == 'create') {
+            $this->btnCssClass = !$this->btnCssClass ? 'btn  btn-sm btn-small btn-primary' : $this->btnCssClass;
+            $this->iconCssClass = !$this->iconCssClass ? 'fas fa-plus' : $this->iconCssClass;
+        }
+        $this->initMyCustomButton();
+    }
 
-	protected function initMyCustomButton() {
-		$this->template                 = "{" . $this->action . "}";
-		$this->buttons[ $this->action ] = function ( $url, $model, $key ) {
+    protected function initMyCustomButton() {
+        $this->template = "{" . $this->action . "}";
+        $this->buttons[$this->action] = function ($url, $model, $key) {
 
-			$data = [];
+            $data = [];
 
-			foreach ( $this->data as $_key => $value ) {
-				$data[ $_key ] = is_callable( $value ) ? $data[ $_key ] = call_user_func( $value, $key ) : $this->data[ $_key ];
-			}
+            foreach ($this->data as $_key => $value) {
+                $data[$_key] = is_callable($value) ? $data[$_key] = call_user_func($value, $key) : $this->data[$_key];
+            }
 
-			if ( is_callable( $this->btnCssClass ) ) {
-				$btnCssClass = call_user_func( $this->btnCssClass, $model );
-			} else {
-				$btnCssClass = $this->btnCssClass;
-			}
+            if (is_callable($this->btnCssClass)) {
+                $btnCssClass = call_user_func($this->btnCssClass, $model);
+            } else {
+                $btnCssClass = $this->btnCssClass;
+            }
 
-			if ( is_callable( $this->title ) ) {
-				$title = call_user_func( $this->title, $model );
-			} else {
-				$title = $this->title;
-			}
+            if (is_callable($this->title)) {
+                $title = call_user_func($this->title, $model);
+            } else {
+                $title = $this->title;
+            }
 
-			//todo: Check if disabled!
-			if ( is_callable( $this->disabled ) ) {
-				$disabled = call_user_func( $this->disabled, $model );
-				if ( $disabled ) {
-					unset( $data['confirm'] );
-					unset( $data['method'] );
-				}
-			}
+            //todo: Check if disabled!
+            if (is_callable($this->disabled)) {
+                $disabled = call_user_func($this->disabled, $model);
+                if ($disabled) {
+                    unset($data['confirm']);
+                    unset($data['method']);
+                }
+            }
 
-			if ( $this->type == 'link' ) {
-				return Html::a( '<i class="' . $this->iconCssClass . '"></i>', ( $disabled ? false : $url ), [
-					'title'    => Yii::t( 'yii', $title ),
-					'data'     => array_merge( [ 'pjax' => 0 ], $data ),
-					'class'    => $btnCssClass,
-					'disabled' => $disabled
-				] );
-			} elseif ( $this->type == 'button' ) {
-				return Html::button( '<i class="' . $this->iconCssClass . '"></i>', [
-					'title'    => Yii::t( 'yii', $title ),
-					'data'     => array_merge( [ 'pjax' => 0 ], $data ),
-					'class'    => $btnCssClass,
-					'disabled' => $disabled
-				] );
-			}
-		};
+            if ($this->type == 'link') {
+                return Html::a('<i class="' . $this->iconCssClass . '"></i>', ($disabled ? false : $url), [
+                    'title'    => Yii::t('yii', $title),
+                    'data'     => array_merge(['pjax' => 0], $data),
+                    'class'    => $btnCssClass,
+                    'disabled' => $disabled
+                ]);
+            } elseif ($this->type == 'button') {
+                return Html::button('<i class="' . $this->iconCssClass . '"></i>', [
+                    'title'    => Yii::t('yii', $title),
+                    'data'     => array_merge(['pjax' => 0], $data),
+                    'class'    => $btnCssClass,
+                    'disabled' => $disabled
+                ]);
+            }
+        };
         return;
-	}
+    }
 }
